f73b9d0ce082080f96fa84eba83ed3e146e733da
Alexey <test@git.ru>
2024-03-29 01:18:02 +0300

    reports

---
+++ app/Http/Controllers/Admin/SpecialMaterialsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\ClientController\StoreMaterialRequest;
+use App\Models\File;
+use App\Services\ClientController\MaterialService;
+use Illuminate\Http\Request;
+
+class SpecialMaterialsController extends Controller
+{
+    public function index()
+    {
+        return File::where('type', 'special')->orderBy('created_at', 'desc')->get();
+    }
+    public function store(StoreMaterialRequest $request)
+    {
+        return (new MaterialService())->store($request, 'special');
+    }
+}
